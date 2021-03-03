5634d8138618ca2e7866cfc96977ce805831b079
Brook Mesfin <brook.mesfin@selerix.com>
2021-03-03 14:52:35 -0600

    Initial Commit

---
+++ components/RecentArticle.vue
@@ -0,0 +1,26 @@
+<template>
+  <article class="col-4">    
+      <div class="mt-4">          
+          <div class="cropped" :style="{ backgroundImage : `url(${thumbnail})`}"></div> 
+      </div>         
+      <p class="lead mt-5 font-weight-bold" v-html="title"></p>
+      <p v-html="subtitle"></p>
+      <button type="button" class="btn btn-outline-primary mt-2 mb-3">More</button>
+      <hr class="mb-4">
+  </article>
+</template>
+
+<script>
+export default {
+    props: {
+        'thumbnail': String,
+        'title': String,
+        'subtitle': String,
+        'crop': String
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,53 @@
+<template>  
+  <div>
+    <section id="mission-statement">      
+      <h4 class="title mt-5">
+        Mission Statement:
+      </h4>
+      <p class="mt-3">
+        The purpose of this site is to serves as a learning platform for young adults mainly living and working outside of Ethiopia and who want to gain knowledge in works related to biological sciences in Ethiopia. It aims at introducing what was done in the past, and what is also being done now, with major focus on botanical sciences. As far as possible manuscripts will be both in English and Amharic.  
+      </p>  
+      <button class="btn btn-outline-danger mt-2 mb-5">Learn More</button>
+      <hr/>
+    </section>  
+
+    <section id="articles" class="mt-4">
+      <p class="lead section-header title">
+        Recent Articles
+      </p>
+      <div class="articles-container row">
+        <recent-article :thumbnail="'/images/IntroducedPlantsInEthiopia/Araucaria.jpg'" :title="'Introduced Plants in Ethiopia'" 
+                        :subtitle="'Images of plants introduced to Ethiopia.'" :crop="'-260px 0px 0px 0px'"/>
+        <recent-article :thumbnail="'/images/EndemicWildAnimals/AwashShewa.jpg'" :title="'Endemic Wild Animals'" 
+                        :subtitle="'Images of endemic and wild Animals of Ethiopia.'" :crop="'0px 0px 0px 0px'"/>
+        <recent-article :thumbnail="'/images/QueerPlantsEthiopia/QueerPlantPicture.jpg'" :title="'Queer Plants of Ethiopia'" 
+                        :subtitle="'Images of queer Ethiopian plants.'" :crop="'0px 0px 0px 0px'"/>
+        <recent-article :thumbnail="'/images/UsefulPlantsOfEthiopia/EnseteVentricosum.jpg'" :title="'Useful Plants of Ethiopia'" 
+                        :subtitle="'Images of plants useful to the inhabitants of Ethiopia.'" :crop="'0px 0px 0px 0px'"/>
+        <recent-article :thumbnail="'/images/EndemicPlantsOfEthiopia/BidensMacroptera.jpg'" :title="'Endemic Plants of Ethiopia'" 
+                        :subtitle="'I have included 4 endemic plants: Bidens macroptera, Echinops kebericho, Plectocephalus varians and Urtica simensis.'" :crop="'0px 0px 0px 0px'"/>
+        <recent-article :thumbnail="'/images/WildBirdsEthiopia/CormorantRiftValley.jpg'" :title="'Endemic and Wild Animals of Ethiopia'" 
+                        :subtitle="'Images of endemic and wild Animals of Ethiopia.'" :crop="'0px 0px 0px 0px'"/>
+      </div>
+    </section>
+  </div>    
+</template>
+
+<script>
+import RecentArticle from '../components/RecentArticle.vue'
+export default {
+    components: { RecentArticle },
+  
+}
+</script>
+
+<style>
+.section-header {
+  display: inline-block;
+  padding-top: 10px;
+  border-bottom: solid 5px #f56a6a; 
+}
+.articles-container {
+  display: flex;
+}
+</style>
